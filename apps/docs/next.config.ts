76f90a2c1393a537984551446dc8f54e98e88c02
Zahin Afsar <afsarzahin@gmail.com>
2026-02-19 22:00:05 +0600

    feat: configure Next.js for production and add GitHub Actions workflow for documentation deployment

--- apps/docs/next.config.ts
+++ apps/docs/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: isProd ? "/uispeaker" : "",
+  assetPrefix: isProd ? "/uispeaker/" : "",
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
